201b8aec9e04fe28ff117a5a84eddff0eaa8f40a
Hoh Wei De <51860691+Ael22@users.noreply.github.com>
2026-09-14 15:10:34 +0800

    Exercise 5.1 - 5.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,129 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [color, setColor] = useState('')
+  const [message, setMessage] = useState(null)
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      window.localStorage.setItem(
+        'loggedBloglistUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+    } catch {
+      console.log('Error logging in')
+      setMessage('Wrong username or password')
+      setColor('red')
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000);
+    }
+  }
+
+  const addBlog = (blogObject) => {
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setMessage(`a new blog ${returnedBlog.title} by ${returnedBlog.author} added`)
+        setColor('green')
+        setTimeout(() => {
+          setMessage(null)
+        }, 3000)
+      })
+  }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+    blogService.setToken(null)
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          <h1>log in to application</h1>
+          <Notification message={message} color={color} />
+        </div>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    )
+  }
+
+  const displayBlogs = () => {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <Notification message={message} color={color} />
+        <p>{user.name} logged in<button onClick={handleLogout}>logout</button></p>
+        <Togglable buttonLabel='create new blog'>
+          <BlogForm createBlog={addBlog} /></Togglable>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {!user && loginForm()}
+      {user && (
+        <>
+          {displayBlogs()}
+        </>
       )}
     </div>
   )
